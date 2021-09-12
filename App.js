a452619fb25067d598f6a769ddb4ac8b182474e8
Simon <simonwatson176@gmail.com>
2021-09-12 17:04:21 +0100

    Platform Specific Code

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Image, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
-  console.log("App Executed");
+  // console.log("App Executed");
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Text>Hello Sergio!!</Text>
+      <TouchableOpacity onPress={()=>{}}>
+      <Image
+            // blurRadius={10}
+            // fadeDuration={1000}
+             source={{ 
+             width:200,
+             height:300,
+             uri:"https://picsum.photos/200/300",}}/>
+             <Button
+               title = "Click Me" 
+               onPress={() =>
+               Alert.prompt("My Title", "My Message", (text) => console.log(text))}/>
+      </TouchableOpacity>
+      
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
+    
   );
 }
+const containerStyle = {backgroundColor:"aqua"};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
